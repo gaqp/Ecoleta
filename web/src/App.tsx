fd1ea07efd676cd2c386b03a6a6a517f34e8c509
gaqp <gaqp@cin.ufpe.br>
2020-06-09 23:32:34 -0300

    Pausa aos 43 minutos da aula 3

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './pages/home/index';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-      </header>
+    <div>
+    <h1>Hello World</h1>
+    <Home/>
     </div>
   );
 }
